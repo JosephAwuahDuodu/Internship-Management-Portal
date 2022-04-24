4eefb4b18cee27b680eb623b4dd15251a67562b7
Awuah Duodu Joseph <awuahduodujoseph@gmail.com>
2022-04-24 20:18:31 +0000

    Added student model, proceeding to create student index page for admin to upload csv

---
+++ database/migrations/2022_04_24_201144_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_id');
+            $table->string('name')->nullable();
+            $table->string('programme');
+            $table->string('phone', 12)->nullable();
+            $table->string('email')->nullable();
+            $table->text('bio')->nullable();
+            $table->boolean('active_status')->default(true);
+            $table->timestamps();
+            $table->softDeletes();;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
