c4d7510f3bfb4dfc69e1071902d686dd88482e44
Awuah Duodu Joseph <awuahduodujoseph@gmail.com>
2022-04-27 21:31:05 +0000

    Simulating student dashboard environment

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -39,86 +39,28 @@
         <div class="col-xl-4">
             <div class="card widget widget-list">
                 <div class="card-header">
-                    <h5 class="card-title">Registered Students<span class="badge badge-success badge-style-light">0 Listed</span></h5>
+                    <h5 class="card-title">Organizations<span class="badge badge-success badge-style-light">{{ count($organizations) ?? "" }} Listed</span></h5>
                 </div>
                 <div class="card-body">
-                    <span class="text-muted m-b-xs d-block">showing 5 out of 23 active tasks.</span>
+                    <span class="text-muted m-b-xs d-block">showing {{ count($organizations) ?? "" }} out of {{ count($organizations) ?? "" }} active records.</span>
                     <ul class="widget-list-content list-unstyled">
-                        <li class="widget-list-item widget-list-item-green">
-                            <span class="widget-list-item-check">
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="checkbox" value="">
-                                </div>
-                            </span>
-                            <span class="widget-list-item-description">
-                                <a href="#" class="widget-list-item-description-title">
-                                    Dashboard UI optimisations
-                                </a>
-                                <span class="widget-list-item-description-subtitle">
-                                    Oskar Hudson
+                        @if ($organizations ?? "")
+                            @forelse ($organizations as $org)
+                            <li class="widget-list-item widget-list-item-green">
+                                <span class="widget-list-item-description">
+                                    <a class="widget-list-item-description-title">
+                                        {{ $org->org_name ?? "Organization Name" }}
+                                    </a>
+                                    <span class="widget-list-item-description-subtitle">
+                                        {{-- {{ Oskar Hudson }} --}}
+                                    </span>
                                 </span>
-                            </span>
-                        </li>
-                        <li class="widget-list-item widget-list-item-blue">
-                            <span class="widget-list-item-check">
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="checkbox" value="" checked>
-                                </div>
-                            </span>
-                            <span class="widget-list-item-description">
-                                <a href="#" class="widget-list-item-description-title">
-                                    Mailbox cleanup
-                                </a>
-                                <span class="widget-list-item-description-subtitle">
-                                    Woodrow Hawkins
-                                </span>
-                            </span>
-                        </li>
-                        <li class="widget-list-item widget-list-item-purple">
-                            <span class="widget-list-item-check">
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="checkbox" value="" checked>
-                                </div>
-                            </span>
-                            <span class="widget-list-item-description">
-                                <a href="#" class="widget-list-item-description-title">
-                                    Header scroll bugfix
-                                </a>
-                                <span class="widget-list-item-description-subtitle">
-                                    Sky Meyers
-                                </span>
-                            </span>
-                        </li>
-                        <li class="widget-list-item widget-list-item-yellow">
-                            <span class="widget-list-item-check">
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="checkbox" value="">
-                                </div>
-                            </span>
-                            <span class="widget-list-item-description">
-                                <a href="#" class="widget-list-item-description-title">
-                                    Localization for file manager
-                                </a>
-                                <span class="widget-list-item-description-subtitle">
-                                    Oskar Hudson
-                                </span>
-                            </span>
-                        </li>
-                        <li class="widget-list-item widget-list-item-red">
-                            <span class="widget-list-item-check">
-                                <div class="form-check form-check-inline">
-                                    <input class="form-check-input" type="checkbox" value="" checked>
-                                </div>
-                            </span>
-                            <span class="widget-list-item-description">
-                                <a href="#" class="widget-list-item-description-title">
-                                    New E-commerce UX/UI design
-                                </a>
-                                <span class="widget-list-item-description-subtitle">
-                                    Oskar Hudson
-                                </span>
-                            </span>
-                        </li>
+                            </li>
+
+                            @empty
+
+                            @endforelse
+                        @endif
                     </ul>
                 </div>
             </div>
--- resources/views/admin/students/_add_student_modal.blade.php
+++ resources/views/admin/students/_add_student_modal.blade.php
@@ -1,85 +1,24 @@
 <!-- Modal -->
-<div class="modal fade" id="add_internship_offer_modal" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-xl">
+<div class="modal fade" id="add_student_modal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-md">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Add New organization</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Add New Students</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form action="{{ route('organizations.store') }}" method="post">
+            <form action="{{ route('organizations.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
-                <div class="modal-body">
+                <div class="modal-body pt-0">
                     <div class="row">
 
-                        <div class="col-md-6">
-                            <div class="form-group mt-3">
-                                <label for="org_name">Organisation Name:* </label>
-                                <input type="text" title="" name="org_name" value="{{ old('name') }}" placeholder="Company Official Name" class="form-control" id="org_name" required>
-                            </div>
-
-                            <div class="form-group mt-3">
-                                <label for="primary_contact">Primary Phone Number:* </label>
-                                <input type="text" pattern="[0-9]{12}" title="Valid Format: 12 Digits Including Country Code" name="primary_contact" value="{{ old('primary_contact') }}" placeholder="Example: 233553220200" class="form-control" id="primary_contact" required>
-                            </div>
-
-                            <div class="form-group mt-3">
-                                <label for="region_id">Region:* </label>
-                                <select name="region_id" id="region_id" class="form-control" required>
-                                    <option value="">Select Regions</option>
-                                    @if ($regions ?? "")
-                                        @foreach ($regions as $region)
-                                            <option value="{{ $region->region_id }}">{{ $region->region_name }}</option>
-                                        @endforeach
-                                    @endif
-                                </select>
-                            </div>
-
-                            <div class="form-group mt-3">
-                                <label for="email">Email: </label>
-                                <input type="email"title="" name="email" value="{{ old('email') }}" placeholder="Company Official Email" class="form-control" id="email" required>
-                            </div>
-
-                        </div>
-
-                        <div class="col-md-6">
-
-                            <div class="form-group mt-3">
-                                <label for="industry_id">Industry: </label>
-                                <select name="industry_id" id="industry_id" class="form-control" required>
-                                    <option value="">Select Industry</option>
-                                    @if ($industries ?? "")
-                                        @foreach ($industries as $industry)
-                                            <option value="{{ $industry->industry_id }}">{{ $industry->name }}</option>
-                                        @endforeach
-                                    @endif
-                                </select>
-                            </div>
-
-                            <div class="form-group mt-3">
-                                <label for="other_contacts">Other Phone Numbers: </label>
-                                <input type="text" title="Valid Format: 12 Digits Including Country Code" name="other_contacts" value="{{ old('other_contacts') }}" placeholder="233553220200, 233553220200" class="form-control" id="other_contacts">
-                            </div>
-
-                            <div class="form-group mt-3">
-                                <label for="city">City: </label>
-                                <input name="city" value="{{ old('city') }}" type="text" class="form-control" id="city" >
-                            </div>
-
-                            <div class="form-group mt-3">
-                                <label for="post_office">P. O Box Office: </label>
-                                <input name="post_office" value="{{ old('post_office') }}" type="text" class="form-control" id="post_office" >
-                            </div>
-
-                            <div class="form-group mt-3">
-                                <label for="address">Address: </label>
-                                <input name="address" value="{{ old('address') }}" type="text" class="form-control" id="address" required>
-                            </div>
-
-                        </div>
-
                         <div class="col-md-12">
-                            <label for="org_desc">Extra Information: </label>
-                            <textarea name="org_desc" class="form-control" id="org_desc" rows="3">{{ old('org_desc') }}</textarea>
+
+                            <a href="{{ asset('static/files/student_list.jpg') }}" style="font-size: 1.2em; font-weight:bold;" type="button" class="text-center text-uppercase btn btn-danger btn-sm" download>Download Sample Sheet</a>
+
+                            <div class="form-group mt-3">
+                                <label for="org_name">Student List File:* </label>
+                                <input type="file" title="Upload Student File" name="students_list" value="{{ old('students_list') }}" placeholder="Student List to be Uploaded" class="form-control" id="students_list" required>
+                            </div>
                         </div>
 
                     </div>
@@ -87,7 +26,7 @@
 
                 </div>
                 <div class="modal-footer">
-                    <input type="submit" class="btn btn-block btn-success" value="Save Organization Details">
+                    <input type="submit" class="btn btn-block btn-success" value="Upload Students Details">
                     <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
                 </div>
             </form>
